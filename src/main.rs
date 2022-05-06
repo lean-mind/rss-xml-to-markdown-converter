7896a38f350b29f83b175071556eb2c134e2483f
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-05-06 12:27:48 +0100

    Read XML POC

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use serde_xml_rs::from_str;
+use std::fs::read_to_string;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct RSSFeed {
+    channel: Channel,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct Channel {
+    title: String,
+}
+
+fn read_xml(filename: &str) -> RSSFeed {
+    from_str(&read_to_string(filename).expect("Can't read the file"))
+        .expect("File with wrong format")
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_read_xml() {
+        assert_eq!(
+            read_xml("example.xml"),
+            RSSFeed {
+                channel: Channel {
+                    title: String::from("Ni cero, ni uno")
+                }
+            }
+        );
+    }
 }
