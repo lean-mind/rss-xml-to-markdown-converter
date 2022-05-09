fc31d9d0cf5da21c22dd2f63e2bd48cf3a114099
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-05-09 11:44:05 +0100

    Add individual podcast information

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,25 @@ struct Channel {
     title: String,
     description: String,
     owner: Owner,
+    #[serde(rename="item")]
+    items: Vec<Item>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct Item {
+    title: String,
+    link: String,
+    #[serde(rename="pubDate")]
+    date: String,
+    #[serde(rename="summary")]
+    description: String,
+    enclosure: Enclosure,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct Enclosure {
+    #[serde(rename="url")]
+    link_mp3: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
@@ -42,7 +61,28 @@ mod tests {
                     owner: Owner {
                         name: String::from("Carlos Blé"),
                         email: String::from("carlos@carlosble.com")
-                    }
+                    },
+                    items: vec![
+                        Item {
+                            title: String::from("E32: ¿Para qué impartir una ponencia?"),
+                            link: String::from("https://podcast.carlosble.com/podcast/e32-para-que-impartir-una-ponencia/"),
+                            date: String::from("Thu, 14 Apr 2022 15:58:24 +0000"),
+                            description: String::from("¿Para qué dar una charla?, ¿por qué exponerse?, ¿qué puedes aportar cuando parece que ya está todo en internet?. En este episodio converso con Adrián Ferrera, Francisco Mesa y Miguel Cabrera. Además, contamos con mensajes grabados de ponentes que me encantan, Yodra López, Jorge J. Barroso, Aida Albarrán y Alexandra Rivero. Contribuciones: Yodra López (1:07:09)..."),
+                            enclosure: Enclosure {
+                                link_mp3: String::from("https://podcast.carlosble.com/podcast-download/1220/e32-para-que-impartir-una-ponencia.mp3"),
+                            }
+                        }, 
+                        Item {
+                            title: String::from("E31: El sendero de la maestría (III)"),
+                            link: String::from("https://podcast.carlosble.com/podcast/e31-el-sendero-de-la-maestria-iii/"),
+                            date: String::from("Fri, 18 Mar 2022 09:43:27 +0000"),
+                            description: String::from("Terminamos la serie de epidosodios sobre aprendizaje (por ahora), con el gran Jose Enrique Rodríguez Huerta, actualmente director de Codurance España. En esta conversación José nos habla de cultura empresarial, liderazgo, carrera profesional, trabajo en equipo y por supuesto, aprendizaje. Recursos citados en el episodio (y otros que olvidé citar): Libro: Tribal Leadership Trabajar en..."),
+                            enclosure: Enclosure {
+                                link_mp3: String::from("https://podcast.carlosble.com/podcast-download/1216/e31-el-sendero-de-la-maestria-iii.mp3"),
+                            }
+                        
+                        }
+                    ]
                 }
             }
         );
