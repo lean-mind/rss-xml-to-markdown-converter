a304714652b353c78301628dc5fd43a4ead5b600
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-05-11 10:11:06 +0100

    Extract some code from main into functions

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod xml_definitions;
 mod yml_definitions;
 
 use clap::Parser;
-use std::fs::{read_to_string, File, create_dir_all};
+use std::fs::{create_dir_all, read_to_string, File};
 use std::io::Write;
+use std::path::Path;
 use xml_definitions::*;
 use yml_definitions::*;
-use std::path::Path;
 
 fn read_rss_feed(filename: &str) -> RSSFeed {
     serde_xml_rs::from_str(&read_to_string(filename).expect("Can't read the RSS file"))
@@ -31,33 +31,40 @@ fn episode_to_hugo_markdown(podcast: &Podcast, episode_index: usize) -> String {
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// The name of a file from which the RSS feed will be readº
+    /// The name of a file from which the RSS feed will be read
     input_filename: String,
     /// Where to save the output. By default a file will be created, but
     /// if separate_by_episodes is set, a directory will be created with
     /// one file per episode
     output_path: String,
     /// If this flag is set, a file will be created for each episode
-    #[clap(short='e', long)]
+    #[clap(short = 'e', long)]
     separate_by_episodes: bool,
 }
 
 fn main() {
     let args = Args::parse();
-
     let podcast = Podcast::from_rss_feed(read_rss_feed(&args.input_filename));
-    if args.separate_by_episodes {
-        let output_path = Path::new(&args.output_path);
-        create_dir_all(output_path).expect("Error creating directory");
-        for episode_index in 0..podcast.episodes.len() {
-            let markdown = episode_to_hugo_markdown(&podcast, episode_index);
-            let filename = "1.md"; // TODO
-            let mut file = File::create(output_path.join(filename)).expect("Error creating file");
-            write!(file, "{}", markdown).expect("Error writing to file");
-        }
-    } else {
-        let markdown = podcast_to_hugo_markdown(&podcast, );
-        let mut file = File::create(args.output_path).expect("Error creating file");
+    let output_path = Path::new(&args.output_path);
+
+    match args.separate_by_episodes {
+        true => generate_episode_files(&podcast, output_path),
+        false => generate_podcast_file(&podcast, output_path),
+    }
+}
+
+fn generate_podcast_file(podcast: &Podcast, output_path: &Path) {
+    let markdown = podcast_to_hugo_markdown(podcast);
+    let mut file = File::create(output_path).expect("Error creating file");
+    write!(file, "{}", markdown).expect("Error writing to file");
+}
+
+fn generate_episode_files(podcast: &Podcast, output_path: &Path) {
+    create_dir_all(output_path).expect("Error creating directory");
+    for episode_index in 0..podcast.episodes.len() {
+        let markdown = episode_to_hugo_markdown(podcast, episode_index);
+        let filename = "1.md"; // TODO
+        let mut file = File::create(output_path.join(filename)).expect("Error creating file");
         write!(file, "{}", markdown).expect("Error writing to file");
     }
 }
